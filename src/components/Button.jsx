3d2c74344025ea3f1378065b2d03f8e869864f61
ajscholz <yoitsscholzy@gmail.com>
2020-08-31 22:19:25 -0400

    add react-spring, improve animations

---
+++ src/components/Button.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import tw, { styled } from "twin.macro"
+import { useSpring, animated } from "react-spring"
+
+const Button = ({ primary, white, children }, props) => {
+  const [state, setState] = useState(false)
+  const scale = useSpring({ transform: state ? "scale(1.05)" : "scale(1)" })
+
+  const AnimatedButtonBase = animated(ButtonBase)
+
+  return (
+    <AnimatedButtonBase
+      primary={primary}
+      white={white}
+      onFocus={() => setState(true)}
+      onBlur={() => setState(false)}
+      onMouseOver={() => setState(true)}
+      onMouseOut={() => setState(false)}
+      style={scale}
+      {...props}
+    >
+      {children}
+    </AnimatedButtonBase>
+  )
+}
+
+export default Button
+
+const ButtonBase = styled.button(({ primary, white }) => [
+  tw`py-3 px-5 mr-3 mt-2 border-2 border-white`,
+  primary
+    ? tw`bg-dgRed border-dgRed text-red-100`
+    : white
+    ? tw`bg-white text-gray-900`
+    : tw`bg-transparent text-white`,
+  tw`rounded-lg font-semibold uppercase`,
+])
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 // import { Link } from "gatsby"
 import LogoNoTag from "./images/LogoNoTag"
 import tw from "twin.macro"
 import { Link } from "gatsby"
 import { MdClose, MdMenu } from "react-icons/md"
+import { useSpring, useTransition, animated } from "react-spring"
 
 const links = [
   { text: "Connect", to: "/connect" },
@@ -13,12 +14,16 @@ const links = [
   { text: "Plan A Visit", to: "/plan-a-visit" },
 ]
 
+const Overlay = tw.div`
+  md:hidden fixed z-40 bg-black bg-opacity-75 inset-0 
+`
+
 const Navigation = tw.nav`
   hidden md:block text-sm font-semibold text-gray-700
 `
 
-const MobileNav = tw.div`
-  md:hidden fixed text-lg top-0 right-0 h-full w-3/4 p-8 flex flex-col items-end bg-white z-50 shadow-lg
+const MobileNav = tw.nav`
+  md:hidden fixed text-lg top-0 right-0 h-full w-3/5 p-8 flex flex-col items-end bg-white z-50
 `
 
 const CloseButton = tw.button`relative top-0 right-0 mb-8 text-2xl`
@@ -30,6 +35,27 @@ const NavLink = tw(Link)`
 
 const Header = () => {
   const [mobileOpen, setMobileOpen] = useState(false)
+
+  // const props = useSpring({ opacity: mobileOpen ? 1 : 0 })
+  const transitions = useTransition(mobileOpen, null, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+  const slideTransitions = useTransition(mobileOpen, null, {
+    from: { position: "absolute", transform: "translateX(100%)" },
+    enter: { transform: "translateX(0)" },
+    leave: { transform: "translateX(100%)" },
+  })
+
+  useEffect(() => {
+    const body = document.getElementsByTagName("body")[0]
+    if (mobileOpen === true) body.setAttribute("class", "overflow-y-hidden")
+    if (mobileOpen === false) body.removeAttribute("class")
+  }, [mobileOpen])
+
+  const AnimatedOverlay = animated(Overlay)
+  const AnimatedMobileNav = animated(MobileNav)
 
   return (
     <header className="h-20 bg-white overflow-hidden shadow-lg z-10">
@@ -44,21 +70,47 @@ const Header = () => {
             </NavLink>
           ))}
         </Navigation>
-        <MenuButton>
-          <MdMenu onClick={() => setMobileOpen(true)} />
+        <MenuButton onClick={() => setMobileOpen(true)}>
+          <MdMenu />
         </MenuButton>
-        {mobileOpen && (
-          <MobileNav>
-            <CloseButton onClick={() => setMobileOpen(false)}>
-              <MdClose />
-            </CloseButton>
-            {links.map(link => (
-              <NavLink key={link.to} to="link.to">
-                {link.text}
-              </NavLink>
-            ))}
-          </MobileNav>
+        {/* {mobileOpen && (
+          <> */}
+        {transitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <AnimatedOverlay
+                key={key}
+                style={props}
+                onClick={() => setMobileOpen(false)}
+                role="button"
+              />
+            )
         )}
+        {/* <AnimatedOverlay style={props} /> */}
+        {slideTransitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <AnimatedMobileNav key={key} style={props}>
+                <CloseButton onClick={() => setMobileOpen(!mobileOpen)}>
+                  <MdClose />
+                </CloseButton>
+                {links.map(link => (
+                  <NavLink
+                    key={link.to}
+                    to="link.to"
+                    onClick={() => setMobileOpen(false)}
+                  >
+                    {link.text}
+                  </NavLink>
+                ))}
+              </AnimatedMobileNav>
+            )
+        )}
+        {/* <AnimatedMobileNav>
+          
+        </MobileNav> */}
+        {/* </>
+        )} */}
       </div>
     </header>
   )
