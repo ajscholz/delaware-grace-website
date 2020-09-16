62cbefb63560e1a957a74238aa3ca7ff0f907940
ajscholz <yoitsscholzy@gmail.com>
2020-09-16 13:29:53 -0400

    fix info chip error

--- src/components/InfoChip.jsx
+++ src/components/InfoChip.jsx
@@ -1,6 +1,6 @@
 import tw from "twin.macro"
 
-const InfoChip = tw.div`inline-block bg-dgBlue rounded-full mb-1 px-2 py-1 text-xs text-blue-100 w-auto
+const InfoChip = tw.div`inline-block bg-dgBlue-500 rounded-full mb-1 px-2 py-1 text-xs text-blue-100 w-auto
 `
 
 export default InfoChip
