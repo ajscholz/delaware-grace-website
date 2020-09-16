e0fc80a454bd8d375738b7b9d6cbb2a6de634311
ajscholz <yoitsscholzy@gmail.com>
2020-09-16 17:53:34 -0400

    fix netlify build error

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react"
 
 export default function useMedia(queries, values, defaultValue) {
   const match = () =>
-    values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue
+    values[
+      queries.findIndex(
+        q => typeof indow !== "undefined" && window.matchMedia(q).matches
+      )
+    ] || defaultValue
   const [value, set] = useState(match)
   useEffect(() => {
     const handler = () => set(match)
