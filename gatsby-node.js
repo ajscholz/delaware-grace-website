0949a68c09ff91c1adee20879d1012e9f447350d
ajscholz <yoitsscholzy@gmail.com>
2020-09-17 10:24:15 -0400

    clean up gatsby-node, update packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,11 @@
 const path = require(`path`)
 const axios = require("axios")
 const { createRemoteFileNode } = require("gatsby-source-filesystem")
-const { O_NOCTTY } = require("constants")
 
 // process vimeo thumbnails to be used by Gatsby Image
 exports.onCreateNode = async ({
   node,
-  actions: { createNode, create },
+  actions: { createNode },
   store,
   cache,
   createNodeId,
@@ -141,22 +140,22 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   })
 }
 
-// Replacing '/' would result in empty string which is invalid
-const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
-// Implement the Gatsby API “onCreatePage”. This is
-// called after every page is created.
+// // Replacing '/' would result in empty string which is invalid
+// const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+// // Implement the Gatsby API “onCreatePage”. This is
+// // called after every page is created.
 
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage, deletePage } = actions
-  const oldPage = Object.assign({}, page)
-  // Remove trailing slash unless page is /
-  page.path = replacePath(page.path)
-  if (page.path !== oldPage.path) {
-    // Replace new page with old page
-    deletePage(oldPage)
-    createPage(page)
-  }
-}
+// exports.onCreatePage = ({ page, actions }) => {
+//   const { createPage, deletePage } = actions
+//   const oldPage = Object.assign({}, page)
+//   // Remove trailing slash unless page is /
+//   page.path = replacePath(page.path)
+//   if (page.path !== oldPage.path) {
+//     // Replace new page with old page
+//     deletePage(oldPage)
+//     createPage(page)
+//   }
+// }
 
 // Define graphql types
 exports.createSchemaCustomization = ({ actions, schema }) => {
