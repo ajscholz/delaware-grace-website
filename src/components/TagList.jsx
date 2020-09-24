d13fba00aa84aac94393580205e1606cd2c6311d
ajscholz <yoitsscholzy@gmail.com>
2020-09-24 09:57:56 -0400

    fix console error in taglist

--- src/components/TagList.jsx
+++ src/components/TagList.jsx
@@ -5,7 +5,7 @@ import tw, { styled, css, theme } from "twin.macro"
 const Item = styled.li(() => [
   tw`text-dgBlue-600 font-semibold text-xs flex items-center before:(mx-2 bg-dgBlue-600 rounded-full mt-px)`,
   css`
-    &:not(:first-child)::before {
+    &:not(:first-of-type)::before {
       display: block;
       content: "";
       height: 3px;
