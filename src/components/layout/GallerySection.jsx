58ccc8bda0a219ef05fcf7f57861cf2775ff2fc5
ajscholz <yoitsscholzy@gmail.com>
2020-10-29 08:03:16 -0400

    fix console warnings

--- src/components/layout/GallerySection.jsx
+++ src/components/layout/GallerySection.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "twin.macro"
+import { graphql } from "gatsby"
 
 import Section from "../Section"
 import Container from "../Container"
