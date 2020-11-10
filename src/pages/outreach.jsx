aa8e0d03e0bc0b1f8619a782d290d2c62bae9200
ajscholz <yoitsscholzy@gmail.com>
2020-11-10 13:17:33 -0500

    Update outreach page

--- src/pages/outreach.jsx
+++ src/pages/outreach.jsx
@@ -19,21 +19,6 @@ const OutreachPage = ({ data }) => {
       />
       <PageBanner banner={page.banner} />
 
-      <Section>
-        <Container>
-          <Padding>
-            {/* <Title>Why Delaware Grace?</Title> */}
-            <p tw="text-center">
-              At Delaware Grace, we are passionate about making God famous and
-              giving Jesus away to a hurting world, whether it is here in
-              Delaware or around the world. That involves meeting physical needs
-              as well as ultimate spiritual needs. We want you to be involved
-              with us in what God is doing around the world.
-            </p>
-          </Padding>
-        </Container>
-      </Section>
-
       <Sections data={page} />
     </>
   )
